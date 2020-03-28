cfb91ce831305969b61cefd94975635015d6e618
Zhenya Tikhonov <masontikhonov@gmail.com>
2020-03-28 20:55:34 +0300

    feat: make menu active while scrolling

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ const smoothScroll = (event) => {
     if (event.target.tagName === 'A') {
         event.preventDefault();
     }
+    window.removeEventListener('scroll', checkPosition);
     const HREF_TARGET_TOP = document.querySelector(event.target.getAttribute('href')).getBoundingClientRect().top;
     const scrollByTop = HREF_TARGET_TOP - 95;
     if (HREF_TARGET_TOP !== 0) {
         window.scrollBy({top: scrollByTop, behavior: 'smooth'});
     } else {
         window.scrollTo({top: 0, behavior: 'smooth'});
-    }   
+    }
+    const addScrollEvent = () => {
+        setTimeout(function() { window.addEventListener('scroll', checkPosition); }, 500);
+    }
+    addScrollEvent();
 }
 
 const randomReorder = (event) => {
@@ -59,6 +64,26 @@ const preventDefault = (event) => {
         event.preventDefault();
 }
 
+const checkPosition = (event) => {
+    for (const section of document.querySelectorAll('main > div')) {
+        const sectionTop = section.getBoundingClientRect().top,
+        sectionBottom = section.getBoundingClientRect().bottom,
+        windowHeight = document.documentElement.clientHeight;
+        if (sectionTop >= 95 && sectionTop <= section.offsetHeight / 2 + 95) {
+            document.querySelectorAll('#main-menu li').forEach(li => li.classList.remove('active'));
+            if (section.id === 'wrapper-slider') {
+                document.querySelectorAll('#main-menu li')[0].classList.add('active');
+            } else {
+                for (const a of document.querySelectorAll('#main-menu li a')) {
+                    if (a.href.includes(section.id)) {
+                        a.closest('li').classList.add('active');
+                    }
+                }
+            }
+        }
+    }
+}
+
 const MAIN_MENU = document.getElementById('main-menu').querySelector('ul');
 MAIN_MENU.addEventListener('click', makeActive);
 MAIN_MENU.addEventListener('click', smoothScroll);
@@ -79,3 +104,5 @@ FORM_SUBMIT.querySelector('.popup .button.close').addEventListener('click', clos
 FORM_SUBMIT.querySelector('.popup .button.ok').addEventListener('click', closeFormSubmit);
 FORM_SUBMIT.querySelector('.popup .background').addEventListener('click', closeFormSubmit);
 document.addEventListener('keyup', event => {if (event.key === "Escape") {closeFormSubmit();}});
+
+window.addEventListener('scroll', checkPosition);
